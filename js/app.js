881d17f2f184ccbd78e117fe4a1e6b4fa24a7261
Jakub Murin <31470133+JacobMu@users.noreply.github.com>
2017-12-07 20:58:11 +0100

    add first files via upload
    uploaded first ver. of the project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//no idea what this is 
+let admin, 
+    name; 
+    
+name = 'John';
+
+admin=name;
+console.log(admin);
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+//open the nav bar
+console.log("Fuck you, world.");
+document.querySelector('.menu').classList.add('hidden');
