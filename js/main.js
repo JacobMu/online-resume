ab898debef36574969ea4f346f99cb94ebdc6040
Jakub Murin <31470133+JacobMu@users.noreply.github.com>
2017-12-07 22:42:30 +0100

    Nav bar functional
    added JS to make the burger nav bar work.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 //open the nav bar
-console.log("Fuck you, world.");
+
 document.querySelector('.menu').classList.add('hidden');
+
+document.querySelector('.menu-open').addEventListener('click', function() {
+    document.querySelector('.menu').classList.remove('hidden');
+    console.log('I work');
+});
+
+document.querySelector('.menu-close').addEventListener('click', function () {
+    document.querySelector('.menu').classList.add('hidden');    
+    console.log('I work');
+});
+
+
+
+var linkMenu = document.querySelector('li');
+
+function closeMenu(e) {
+    document.querySelector('.menu').classList.add('hidden');
+    
+}
+
+linkMenu.addEventListener('click', closeMenu);
