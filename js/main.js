a6243e0982fb4cca5f37a089bbe73a444157ced4
JacobMu <jackob156@gmail.com>
2018-03-03 22:03:03 +0100

    update js files

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+//open the nav bar 
+const MENU = document.querySelector('.menu');
+const MENUOPEN = document.querySelector('.menu-open');
+const MENUCLOSED = document.querySelector('.menu-close');
+
+MENUOPEN.addEventListener('click', () => {    
+    MENU.classList.remove('hidden');
+});
+MENUCLOSED.addEventListener('click', () => {
+    MENU.classList.add('hidden');
+});
+
+//change clients texts
+
+let slideIndex = 0;
+let slides = document.getElementsByClassName("carousel-item");
+
+function showSlidesCarousel() {    
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";      
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    } 
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlidesCarousel, 6000);
+}
+showSlidesCarousel();
